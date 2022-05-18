ea6b005cf50c9c8d9f5113158de55625264f011d
Dennis van Gils <37263907+Dennis-van-Gils@users.noreply.github.com>
2022-05-18 22:49:44 +0200

    Frequency to resistance best-fit transformation

--- src_mcu/src/main.cpp
+++ src_mcu/src/main.cpp
@@ -4,22 +4,29 @@
   measurement.
 
   Dennis van Gils
-  14-05-2022
+  18-05-2022
 *******************************************************************************/
 
 #include <Arduino.h>
+#include <math.h>
 
 #define Ser Serial
 
 // 32u4 microcontroller has interrupts on digital pins: 0, 1, 2, 3, 7
 // SAMD microcontroller has interrupts on all digital pins, except 4
-#define PIN_SENSOR 7
+#define PIN_SENSOR 5
 #define N_AVG 10    // Number of up-flanks to average over
 #define TIMEOUT 300 // [ms] Timeout to stop detecting the frequency
 
 volatile bool isr_done = false;
 volatile uint8_t isr_counter = 0;
 volatile uint16_t frequency = 0; // [Hz] Measured average frequency
+
+float resistance = 0; // [Ohm] Frequency to resistance best-fit transformation
+const float k = -0.55295297;
+const float x0 = 1.6795268;
+const float b = 0.40662999;
+const float a = 3.98202594;
 
 void isr_rising() {
   /* Interrupt service routine for when an up-flank is detected on the input pin
@@ -43,6 +50,19 @@ bool measure_frequency() {
   isr_counter = 0;
   isr_done = false;
   while ((!isr_done) && ((millis() - t_0) < TIMEOUT)) {}
+
+  if (isr_done) {
+    // Frequency to resistance best-fit transformation (is monotonic, yeay :))
+    // Resistance fit +/-20 % (at worst) wrt as measured during calibration,
+    // good enough for order-of-10s estimation
+    float p = (log10(frequency) - x0) * b;
+    float R_log = (k * log(p / (1 - p)) + a);
+    resistance = pow(10, R_log);
+
+  } else {
+    resistance = NAN;
+  }
+
   return isr_done;
 }
 
@@ -72,7 +92,9 @@ void loop() {
     tick = now;
 
     if (measure_frequency()) {
-      Ser.println(frequency);
+      Ser.print(frequency);
+      Ser.print("\t");
+      Ser.println(resistance);
     } else {
       Ser.println("Timed out");
     }
